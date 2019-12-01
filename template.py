f85db15fc9ade3e6ad3c965be16d7653ea9d484a
Andrew Simon <asimon@domeng.com>
2019-12-01 12:34:13 -0500

    Allow for part 2

--- template.py
+++ template.py
@@ -1,19 +1,21 @@
 from utils import get_input
 
 
-def main(directory):
+def main(directory, part_2=False):
     input_text = get_input(directory)
     return input_text
 
 
 if __name__ == "__main__":
-    expected_test = int(get_input("outputs_test"))
-    answer_test = main("inputs_test")
-    if expected_test == answer_test:
-        print("Test input successful.")
-        answer = main("inputs")
-        print(f"The answer is {answer}")
-    else:
-        print("Test input was not successful!")
-        print(f"Expected answer:\t{expected_test}")
-        print(f"Calculated answer:\t{answer_test}")
+    for ind in (0, 1):
+        expected_test = int(get_input("outputs_test").splitlines()[ind])
+        answer_test = main("inputs_test", ind)
+        print(f"\nPart {ind+1}")
+        if expected_test == answer_test:
+            print("Test input successful.")
+            answer = main("inputs", ind)
+            print(f"The answer is {answer}")
+        else:
+            print("Test input was not successful!")
+            print(f"Expected answer:\t{expected_test}")
+            print(f"Calculated answer:\t{answer_test}")
