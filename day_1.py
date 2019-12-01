1e43b2cc25142e8f52744c8818abe8dcc7ca730d
Andrew Simon <asimon@domeng.com>
2019-12-01 12:34:39 -0500

    Day 1 part 2

--- day_1.py
+++ day_1.py
@@ -1,22 +1,30 @@
 from utils import get_input
 
 
-def main(directory):
+def main(directory, part_2=False):
     input_text = get_input(directory)
     counter = 0
     for line in input_text.splitlines():
-        counter += int(line) // 3 - 2
+        fuel_mass = int(line) // 3 - 2
+        if part_2:
+            while fuel_mass > 0:
+                counter += fuel_mass
+                fuel_mass = fuel_mass // 3 - 2
+        else:
+            counter += fuel_mass
     return counter
 
 
 if __name__ == "__main__":
-    expected_test = int(get_input("outputs_test"))
-    answer_test = main("inputs_test")
-    if expected_test == answer_test:
-        print("Test input successful.")
-        answer = main("inputs")
-        print(f"The answer is {answer}")
-    else:
-        print("Test input was not successful!")
-        print(f"Expected answer:\t{expected_test}")
-        print(f"Calculated answer:\t{answer_test}")
+    for ind in (0, 1):
+        expected_test = int(get_input("outputs_test").splitlines()[ind])
+        answer_test = main("inputs_test", ind)
+        print(f"\nPart {ind+1}")
+        if expected_test == answer_test:
+            print("Test input successful.")
+            answer = main("inputs", ind)
+            print(f"The answer is {answer}")
+        else:
+            print("Test input was not successful!")
+            print(f"Expected answer:\t{expected_test}")
+            print(f"Calculated answer:\t{answer_test}")
