49c8bfae9a82d5c81cc25825d022a857c8483fe0
Andrew Simon <asimon@domeng.com>
2019-12-02 12:51:21 -0500

    Extract run_main

--- day_1.py
+++ day_1.py
@@ -1,4 +1,4 @@
-from utils import get_input
+from utils import get_input, run_main
 
 
 def main(directory, part_2=False):
@@ -16,15 +16,4 @@ def main(directory, part_2=False):
 
 
 if __name__ == "__main__":
-    for ind in (0, 1):
-        expected_test = int(get_input("outputs_test").splitlines()[ind])
-        answer_test = main("inputs_test", ind)
-        print(f"\nPart {ind+1}")
-        if expected_test == answer_test:
-            print("Test input successful.")
-            answer = main("inputs", ind)
-            print(f"The answer is {answer}")
-        else:
-            print("Test input was not successful!")
-            print(f"Expected answer:\t{expected_test}")
-            print(f"Calculated answer:\t{answer_test}")
+    run_main(main)
--- utils.py
+++ utils.py
@@ -23,3 +23,18 @@ def get_input(directory, base=sys.argv[0].split(".")[0] + ".*"):
         raise FileNotFoundError(path)
     with open(filename, "r") as f:
         return f.read()
+
+
+def run_main(main):
+    for ind in (0, 1):
+        expected_test = int(get_input("outputs_test").splitlines()[ind])
+        answer_test = main("inputs_test", ind)
+        print(f"\nPart {ind+1}")
+        if expected_test == answer_test:
+            print("Test input successful.")
+            answer = main("inputs", ind)
+            print(f"The answer is {answer}")
+        else:
+            print("Test input was not successful!")
+            print(f"Expected answer:\t{expected_test}")
+            print(f"Calculated answer:\t{answer_test}")
