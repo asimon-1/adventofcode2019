d5d3c57262a0864afe2b1b8314e345b5d5590510
Andrew Simon <asimon@domeng.com>
2019-12-02 13:38:13 -0500

    Day 2 part 2

--- day_2.py
+++ day_2.py
@@ -1,21 +1,59 @@
 from utils import get_input, run_main
+from itertools import product
 
 
-def main(directory, part_2=False):
-    input_text = get_input(directory)
-    intcode = [int(x) for x in input_text.split(",")]
-    for ind, opcode in enumerate(intcode):
-        if ind % 4 == 0:
-            if opcode == 1:
-                # Addition
-                intcode[intcode[ind + 3]] = intcode[intcode[ind + 1]] + intcode[intcode[ind + 2]]
-            if opcode == 2:
-                # Multiplication
-                intcode[intcode[ind + 3]] = intcode[intcode[ind + 1]] * intcode[intcode[ind + 2]]
-            if opcode == 99:
-                # Break
-                break
+def run_opcode(intcode, noun, verb):
+    intcode = intcode.copy()
+    pointer = 0
+    opcode = intcode[pointer]
+
+    # Replace noun and verb
+    intcode[1] = noun
+    intcode[2] = verb
+
+    # Run the intcode
+    while opcode != 99:
+        if opcode == 1:
+            # Addition
+            param1 = intcode[pointer + 1]
+            param2 = intcode[pointer + 2]
+            param3 = intcode[pointer + 3]
+            intcode[param3] = intcode[param1] + intcode[param2]
+            pointer += 4
+        elif opcode == 2:
+            # Multiplication
+            param1 = intcode[pointer + 1]
+            param2 = intcode[pointer + 2]
+            param3 = intcode[pointer + 3]
+            intcode[param3] = intcode[param1] * intcode[param2]
+            pointer += 4
+        opcode = intcode[pointer]
+
     return intcode[0]
+
+
+def main(directory, test=False, part_2=False):
+    input_text = get_input(directory)
+    original_intcode = [int(x) for x in input_text.split(",")]
+    if not part_2:
+        if not test:
+            noun = 12
+            verb = 2
+        else:
+            noun = 9
+            verb = 10
+        return run_opcode(original_intcode, noun, verb)
+    else:
+        if not test:
+            # Try every combination from (0,0) to (99,99)
+            for (noun, verb) in product(range(100), range(100)):
+                if run_opcode(original_intcode, noun, verb) == 19690720:
+                    # Success
+                    break
+        else:
+            noun = 12
+            verb = 2
+        return 100 * noun + verb
 
 
 if __name__ == "__main__":
