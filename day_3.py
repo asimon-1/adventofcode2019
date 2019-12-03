ae38d22b98074a2d62f71de6e9d42e74e1cffa54
Andrew Simon <asimon@domeng.com>
2019-12-03 12:58:50 -0500

    Day 3 part 1 rework

--- day_3.py
+++ day_3.py
@@ -8,10 +8,10 @@ def main(directory, test=False, part_2=False):
     wire1 = wire1.split(",")
     wire2 = wire2.split(",")
     
-    mask1 = np.full((100000,100000), False)
-    mask2 = np.full((100000,100000), False)
-    coords1 = [50000,50000]
-    coords2 = [50000,50000]
+    mask1 = set()
+    mask2 = set()
+    coords1 = [0,0]
+    coords2 = [0,0]
     for (instr1, instr2) in zip(wire1, wire2):
         dir1 = instr1[0]
         step1 = int(instr1[1:])
@@ -19,7 +19,7 @@ def main(directory, test=False, part_2=False):
         step2 = int(instr2[1:])
 
         for _ in range(step1):
-            mask1[coords1[0], coords1[1]] = True
+            mask1.add(tuple(coords1))
             if dir1 == "U":
                 coords1[1] += 1
             elif dir1 == "R":
@@ -29,7 +29,7 @@ def main(directory, test=False, part_2=False):
             elif dir1 == "L":
                 coords1[0] -= 1
         for _ in range(step2):
-            mask2[coords2[0], coords2[1]] = True
+            mask2.add(tuple(coords2))
             if dir2 == "U":
                 coords2[1] += 1
             elif dir2 == "R":
@@ -38,16 +38,11 @@ def main(directory, test=False, part_2=False):
                 coords2[1] -= 1
             elif dir2 == "L":
                 coords2[0] -= 1
-    intersections = np.nonzero(np.logical_and(mask1, mask2))
-    print(intersections)
-    
-    distances = (abs(intersections[0] - 50000) + abs(intersections[1] - 50000))
+    intersections = mask1.intersection(mask2)
+    distances = {abs(inter[0]) + abs(inter[1]) for inter in intersections if inter != (0,0)}
     print(distances)
 
-    return min(distances[distances > 1])
-
-    
-
+    return min(distances)
 
 if __name__ == "__main__":
     run_main(main)
