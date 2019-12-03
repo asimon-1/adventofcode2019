997a57739158d3607a713054556aba25b9be0361
Andrew Simon <asimon@domeng.com>
2019-12-03 18:32:16 -0500

    Day 3 part 2

--- day_3.py
+++ day_3.py
@@ -1,46 +1,55 @@
 from utils import get_input, run_main
-import numpy as np
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Wire:
+    instructions: tuple
+    dir_map: dict = field(default_factory=lambda: {"U": (0, 1), "R": (1, 0), "D": (0, -1), "L": (-1, 0)})
+    current_instruction: int = 0
+    path: list = field(default_factory=lambda: [(0, 0)])
+
+    def execute_next_instruction(self):
+        direction = self.instructions[self.current_instruction][0]
+        additional_steps = self.instructions[self.current_instruction][1]
+        last_position = self.path[-1]
+
+        additional_path = [
+            (
+                last_position[0] + (_ + 1) * self.dir_map[direction][0],
+                last_position[1] + (_ + 1) * self.dir_map[direction][1],
+            )
+            for _ in range(additional_steps)
+        ]
+        self.path += additional_path
+        self.current_instruction += 1
+
+    def execute_all_instructions(self):
+        while self.current_instruction < len(self.instructions):
+            self.execute_next_instruction()
 
 
 def main(directory, test=False, part_2=False):
     input_text = get_input(directory)
     (wire1, wire2) = input_text.splitlines()
-    wire1 = wire1.split(",")
-    wire2 = wire2.split(",")
+    wire1_instructions = tuple((instr[0], int(instr[1:])) for instr in wire1.split(","))
+    wire2_instructions = tuple((instr[0], int(instr[1:])) for instr in wire2.split(","))
 
-    mask1 = set()
-    mask2 = set()
-    coords1 = [0, 0]
-    coords2 = [0, 0]
-    for (instr1, instr2) in zip(wire1, wire2):
-        dir1 = instr1[0]
-        step1 = int(instr1[1:])
-        dir2 = instr2[0]
-        step2 = int(instr2[1:])
+    wire1 = Wire(wire1_instructions)
+    wire2 = Wire(wire2_instructions)
 
-        for _ in range(step1):
-            mask1.add(tuple(coords1))
-            if dir1 == "U":
-                coords1[1] += 1
-            elif dir1 == "R":
-                coords1[0] += 1
-            elif dir1 == "D":
-                coords1[1] -= 1
-            elif dir1 == "L":
-                coords1[0] -= 1
-        for _ in range(step2):
-            mask2.add(tuple(coords2))
-            if dir2 == "U":
-                coords2[1] += 1
-            elif dir2 == "R":
-                coords2[0] += 1
-            elif dir2 == "D":
-                coords2[1] -= 1
-            elif dir2 == "L":
-                coords2[0] -= 1
-    intersections = mask1.intersection(mask2)
-    distances = {abs(inter[0]) + abs(inter[1]) for inter in intersections if inter != (0, 0)}
-    print(distances)
+    wire1.execute_all_instructions()
+    wire2.execute_all_instructions()
+
+    intersections = set(wire1.path).intersection(set(wire2.path))
+
+    if not part_2:
+        # Manhattan Distance
+        distances = {abs(inter[0]) + abs(inter[1]) for inter in intersections if inter != (0, 0)}
+
+    else:
+        # Minimum steps
+        distances = {wire1.path.index(inter) + wire2.path.index(inter) for inter in intersections if inter != (0, 0)}
 
     return min(distances)
 
