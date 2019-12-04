0c21d99fa6a3d720927d3463529e4fbe73b82f6d
Andrew Simon <asimon@domeng.com>
2019-12-04 18:01:39 -0500

    Day 4 part 2

--- day_4.py
+++ day_4.py
@@ -3,26 +3,30 @@ from math import log10
 
 
 def check_increasing(n):
-    digits = int(log10(n)) + 1
-    n_digits = [n // 10 ** e % 10 for e in range(digits - 1, -1, -1)]
-    if all((n_digits[i + 1] - n_digits[i] >= 0 for i in range(len(n_digits) - 1))):
-        return True
-    return False
+    num_digits = int(log10(n)) + 1
+    n_list = [n // 10 ** e % 10 for e in range(num_digits - 1, -1, -1)]
+    return all((n_list[i + 1] - n_list[i] >= 0 for i in range(len(n_list) - 1)))
 
 
-def check_adjacent(n):
-    digits = int(log10(n)) + 1
-    n_digits = [n // 10 ** e % 10 for e in range(digits - 1, -1, -1)]
-    if any((n_digits[i + 1] == n_digits[i] for i in range(len(n_digits) - 1))):
-        return True
-    return False
+def check_adjacent(n, part_two=False):
+    num_digits = int(log10(n)) + 1
+    n_list = [n // 10 ** e % 10 for e in range(num_digits - 1, -1, -1)]
+    digit_dict = {d: 0 for d in range(10)}
+    for digit in digit_dict:
+        while digit in n_list:
+            n_list.remove(digit)
+            digit_dict[digit] += 1
+    if not part_two:
+        return any({d > 1 for d in digit_dict.values()})
+    else:
+        return 2 in digit_dict.values()
 
 
 def main(directory, test=False, part_2=False):
     input_text = get_input(directory)
     lower_bound = int(input_text.split("-")[0])
     upper_bound = int(input_text.split("-")[1])
-    passwords = {n for n in range(lower_bound, upper_bound) if check_adjacent(n) and check_increasing(n)}
+    passwords = {n for n in range(lower_bound, upper_bound) if check_adjacent(n, part_2) and check_increasing(n)}
     return len(passwords)
 
 
