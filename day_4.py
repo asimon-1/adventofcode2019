51df33b04706a8548df34eac68b7c5b69a48a3fb
Andrew Simon <asimon@domeng.com>
2019-12-04 13:57:28 -0500

    Day 4 part 1

---
+++ day_4.py
@@ -0,0 +1,30 @@
+from utils import get_input, run_main
+from numpy import log10, array
+
+
+def check_increasing(n):
+    digits = int(log10(n)) + 1
+    n_digits = array([n // 10 ** e % 10 for e in range(digits - 1, -1, -1)])
+    if all((n_digits[i + 1] - n_digits[i] >= 0 for i in range(len(n_digits) - 1))):
+        return True
+    return False
+
+
+def check_adjacent(n):
+    digits = int(log10(n)) + 1
+    n_digits = array([n // 10 ** e % 10 for e in range(digits - 1, -1, -1)])
+    if any((n_digits[i + 1] == n_digits[i] for i in range(len(n_digits) - 1))):
+        return True
+    return False
+
+
+def main(directory, test=False, part_2=False):
+    input_text = get_input(directory)
+    lower_bound = int(input_text.split("-")[0])
+    upper_bound = int(input_text.split("-")[1])
+    passwords = {n for n in range(lower_bound, upper_bound) if check_adjacent(n) and check_increasing(n)}
+    return len(passwords)
+
+
+if __name__ == "__main__":
+    run_main(main)
