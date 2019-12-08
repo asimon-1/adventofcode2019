a1e0abc11aeb361ae23d7eb72200e65db8c0fa40
Andrew Simon <asimon@domeng.com>
2019-12-08 10:02:58 -0500

    Day 5 part 2

--- day_5.py
+++ day_5.py
@@ -2,7 +2,10 @@ from utils import get_input, run_main
 from itertools import product
 
 
-def addition(intcode, param1, param2, param3, mode1, mode2, mode3):
+def addition(intcode, pointer, mode1, mode2, mode3):
+    param1 = intcode[pointer + 1]
+    param2 = intcode[pointer + 2]
+    param3 = intcode[pointer + 3]
     val1 = param1 if mode1 else intcode[param1]
     val2 = param2 if mode2 else intcode[param2]
 
@@ -12,10 +15,13 @@ def addition(intcode, param1, param2, param3, mode1, mode2, mode3):
     else:
         # Position
         intcode[param3] = val1 + val2
-    return intcode
+    return intcode, pointer + 4
 
 
-def multiplication(intcode, param1, param2, param3, mode1, mode2, mode3):
+def multiplication(intcode, pointer, mode1, mode2, mode3):
+    param1 = intcode[pointer + 1]
+    param2 = intcode[pointer + 2]
+    param3 = intcode[pointer + 3]
     val1 = param1 if mode1 else intcode[param1]
     val2 = param2 if mode2 else intcode[param2]
     if mode3:
@@ -24,17 +30,60 @@ def multiplication(intcode, param1, param2, param3, mode1, mode2, mode3):
     else:
         # Position
         intcode[param3] = val1 * val2
-    print(intcode)
-    return intcode
+    return intcode, pointer + 4
 
 
-def input(intcode, param1, inp):
+def input(intcode, pointer, inp):
+    param1 = intcode[pointer + 1]
     intcode[param1] = inp
-    return intcode
+    return intcode, pointer + 2
 
 
-def output(intcode, param1, mode1):
-    return param1 if mode1 else intcode[param1]
+def output(intcode, pointer, mode1):
+    param1 = intcode[pointer + 1]
+    return intcode, pointer + 2, (param1 if mode1 else intcode[param1])
+
+
+def jump_if_true(intcode, pointer, mode1, mode2):
+    param1 = intcode[pointer + 1]
+    param2 = intcode[pointer + 2]
+    val1 = param1 if mode1 else intcode[param1]
+    val2 = param2 if mode2 else intcode[param2]
+    return intcode, (val2 if val1 else pointer + 3)
+
+
+def jump_if_false(intcode, pointer, mode1, mode2):
+    param1 = intcode[pointer + 1]
+    param2 = intcode[pointer + 2]
+    val1 = param1 if mode1 else intcode[param1]
+    val2 = param2 if mode2 else intcode[param2]
+    return intcode, (val2 if not val1 else pointer + 3)
+
+
+def less_than(intcode, pointer, mode1, mode2):
+    param1 = intcode[pointer + 1]
+    param2 = intcode[pointer + 2]
+    param3 = intcode[pointer + 3]
+    val1 = param1 if mode1 else intcode[param1]
+    val2 = param2 if mode2 else intcode[param2]
+    if val1 < val2:
+        intcode[param3] = 1
+    else:
+        intcode[param3] = 0
+    return intcode, pointer + 4
+
+
+def equal(intcode, pointer, mode1, mode2):
+    param1 = intcode[pointer + 1]
+    param2 = intcode[pointer + 2]
+    param3 = intcode[pointer + 3]
+    val1 = param1 if mode1 else intcode[param1]
+    val2 = param2 if mode2 else intcode[param2]
+    if val1 == val2:
+        intcode[param3] = 1
+    else:
+        intcode[param3] = 0
+    return intcode, pointer + 4
 
 
 def run_opcode(intcode, inp):
@@ -50,24 +99,28 @@ def run_opcode(intcode, inp):
         mode3 = opcode // 10 ** 4 % 10
         if operation == 1:
             # Addition
-            intcode = addition(
-                intcode, intcode[pointer + 1], intcode[pointer + 2], intcode[pointer + 3], mode1, mode2, mode3,
-            )
-            pointer += 4
+            intcode, pointer = addition(intcode, pointer, mode1, mode2, mode3)
         elif operation == 2:
             # Multiplication
-            intcode = multiplication(
-                intcode, intcode[pointer + 1], intcode[pointer + 2], intcode[pointer + 3], mode1, mode2, mode3,
-            )
-            pointer += 4
+            intcode, pointer = multiplication(intcode, pointer, mode1, mode2, mode3)
         elif operation == 3:
             # Input
-            intcode = input(intcode, intcode[pointer + 1], inp)
-            pointer += 2
+            intcode, pointer = input(intcode, pointer, inp)
         elif operation == 4:
             # Output
-            out = output(intcode, intcode[pointer + 1], mode1)
-            pointer += 2
+            intcode, pointer, out = output(intcode, pointer, mode1)
+        elif operation == 5:
+            # Jump-if-true
+            intcode, pointer = jump_if_true(intcode, pointer, mode1, mode2)
+        elif operation == 6:
+            # Jump if false
+            intcode, pointer = jump_if_false(intcode, pointer, mode1, mode2)
+        elif operation == 7:
+            # Less than
+            intcode, pointer = less_than(intcode, pointer, mode1, mode2)
+        elif operation == 8:
+            # Equals
+            intcode, pointer = equal(intcode, pointer, mode1, mode2)
 
         opcode = intcode[pointer]
 
@@ -76,11 +129,23 @@ def run_opcode(intcode, inp):
 
 def main(directory, test=False, part_2=False):
     input_text = get_input(directory)
-    original_intcode = [int(x) for x in input_text.split(",")]
-    if not part_2:
-        return run_opcode(original_intcode, 1)
+    if test:
+        if not part_2:
+            original_intcode = [int(x) for x in input_text.splitlines()[0].split(",")]
+        else:
+            original_intcode = [int(x) for x in input_text.splitlines()[1].split(",")]
     else:
-        return
+        original_intcode = [int(x) for x in input_text.split(",")]
+
+    if not part_2:
+        inp = 1
+    else:
+        if test:
+            inp = 8
+        else:
+            inp = 5
+
+    return run_opcode(original_intcode, inp)
 
 
 if __name__ == "__main__":
