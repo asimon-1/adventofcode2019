31ae13e747be64d49e070f6cc6aef54425395484
Andrew Simon <asimon@domeng.com>
2019-12-08 10:22:14 -0500

    Day 6 part 2

--- day_6.py
+++ day_6.py
@@ -5,6 +5,7 @@ class Obj:
     def __init__(self, parent_name=None):
         self.parent = None
         self.parent_name = parent_name
+        self.parents = []
 
     def update_parent(self, container):
         if self.parent_name is not None:
@@ -16,20 +17,33 @@ class Obj:
         except AttributeError:
             return 0
 
+    def record_parents(self):
+        obj = self
+        while obj.parent:
+            self.parents.append(obj.parent)
+            obj = obj.parent
+
 
 def main(directory, test=False, part_2=False):
     input_text = get_input(directory)
 
+    # Instantiate all objects
+    container = {line.split(")")[1]: Obj(line.split(")")[0]) for line in input_text.splitlines()}
+    container.update({"COM": Obj(None)})
+
+    # Link all objects
+    [obj.update_parent(container) for obj in container.values()]
+
     if not part_2:
-        # Instantiate all objects
-        container = {line.split(")")[1]: Obj(line.split(")")[0]) for line in input_text.splitlines()}
-        container.update({"COM": Obj(None)})
-
-        # Link all objects
-        [obj.update_parent(container) for obj in container.values()]
-
         # Count Orbits
-        return sum([o.count_orbits() for o in container.values()])
+        if test:
+            return sum([o.count_orbits() for k,o in container.items() if k not in ("YOU", "SAN")])
+        else:
+            return sum([o.count_orbits() for o in container.values()])
+
+    else:
+        [obj.record_parents() for obj in container.values()]
+        return len(set(container["SAN"].parents) ^ set(container["YOU"].parents))
 
 
 if __name__ == "__main__":
